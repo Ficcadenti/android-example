802ab9e6683ebf49e38b4a55567b265ca06dc32a
Raffaele Ficcadenti <r.ficcadenti@exolab.app>
2023-02-21 12:29:41 +0100

    Avanzamento

--- Counter/app/src/main/java/com/example/counter/MainActivity.kt
+++ Counter/app/src/main/java/com/example/counter/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.counter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +12,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnBtn1 = findViewById<Button>(R.id.idBtn1)
+        val txtTxt1 = findViewById<TextView>(R.id.idTxt1)
+        var counter :Int = 0;
+
+        txtTxt1.text=counter.toString()
+
         btnBtn1.setOnClickListener {
-            btnBtn1.text="Ciao !!!!!"
+            counter++
+            txtTxt1.text="Persone: ${counter.toString()}"
+            Toast.makeText(this,"Sono un toaST !!!",Toast.LENGTH_LONG).show()
         }
     }
 }
